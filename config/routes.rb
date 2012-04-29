7a6dece369b2db0f981c547ccdc366774e718e49
Mark Connell <mark.connell@edgecase.com>
2012-04-29 03:45:45 +0100

    dont requre authentication for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Sketchpad::Application.routes.draw do
   devise_for :users
 
-  match '/:id' => 'high_voltage/pages#show', :as => :static, :via => :get
-  root :to => 'high_voltage/pages#show', :id => 'home'
+  match '/:id' => 'pages#show', :as => :static, :via => :get
+  root :to => 'pages#show', :id => 'home'
 end
