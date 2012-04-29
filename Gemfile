5b0018a1ea2dd9b516d4958d612b1f25adedc54f
Mark Connell <mark.connell@edgecase.com>
2012-04-29 03:41:10 +0100

    devise

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'sqlite3'
 gem 'jquery-rails'
 gem 'haml'
 gem 'high_voltage'
+gem 'devise'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Sketchpad::Application.routes.draw do
+  devise_for :users
+
   match '/:id' => 'high_voltage/pages#show', :as => :static, :via => :get
   root :to => 'high_voltage/pages#show', :id => 'home'
 end
