2e2c3d132db809bbef07b0557cf9ad424eab5474
Mark Connell <mark.connell@edgecase.com>
2012-05-16 21:00:37 +0100

    brochure page.

--- app/controllers/whiteboards_controller.rb
+++ app/controllers/whiteboards_controller.rb
@@ -1,7 +1,12 @@
 class WhiteboardsController < ApplicationController
+  skip_before_filter :authenticate_user!, :only => :index
 
   def index
-    @whiteboards = current_user.whiteboards.order('updated_at DESC')
+    if user_signed_in?
+      @whiteboards = current_user.whiteboards.order('updated_at DESC')
+    else
+      render "pages/brochure"
+    end
   end
 
   def show
